c7df7f7a69411e7470c3e074537dc8b4fc05a744
KerttuRungi <kerttu.rungi@gmail.com>
2026-01-26 18:14:05 +0200

    Fix navigation button styling

--- frontend-site/recipeblogfrontend/src/components/AccountLogedIn.vue
+++ frontend-site/recipeblogfrontend/src/components/AccountLogedIn.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="text-align:center; margin-top:50px;">
     <h1>Successfully logged in!</h1>
-    <button class="btn btn-success py-2 fw-bold rounded-3 border-0" @click="$router.push('/recipes')">View Recipes</button>
+    <button  class="btn btn-outline-light px-4 rounded-pill" style="background-color: #606c38;" @click="$router.push('/recipes')">View Recipes</button>
   </div>
 </template>
 
